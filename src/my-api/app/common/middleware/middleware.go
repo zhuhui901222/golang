cdaf7ae2fc686e00ccef9b142f7d77246c1744d0
zhuhui901222 <15812902593@139.com>
2019-04-09 14:48:34 +0800

    中间件

--- src/my-api/app/common/middleware/middleware.go
+++ src/my-api/app/common/middleware/middleware.go
@@ -1,14 +1,26 @@
 package middleware
 
-import "github.com/astaxie/beego"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/astaxie/beego"
+)
 import "github.com/astaxie/beego/context"
 
 func init()  {
 	var FilterUser = func(ctx *context.Context) {
-		//	_, ok := ctx.Input.Session("uid").(int)
-		//	if !ok && ctx.Request.RequestURI != "/login" {
 		if ctx.Request.RequestURI != "/login" {
-			ctx.Redirect(404, "/")
+			ctx.Input.SetData("test","123")
+			fmt.Println(ctx.Input)
+			//	Ctx.Output.JSON(c.Data["json"], hasIndent, hasEncoding)
+			res:=map[string]interface{}{"code": 404,"msg": "未登录","data": nil}
+			b, err := json.Marshal(res)
+			if err != nil {
+				fmt.Println("json.Marshal failed:", err)
+				return
+			}
+			ctx.WriteString(string(b))
+		//	ctx.Redirect(404, "/")
 		}
 	}
 	beego.InsertFilter("/v1/object/*",beego.BeforeRouter,FilterUser)
